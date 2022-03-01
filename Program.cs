922a566eba883880ff5feed6913f2e50d8ba2e28
Shamal Kumarathunga <91006682+pasisham@users.noreply.github.com>
2022-03-01 23:29:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonClass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            Single obj1 = Single.getInsatance();
+            
+            Console.ReadLine();
+                
+        }
+    }
+}
---
+++ Single.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonClass
+{
+    internal class Single
+    {
+
+        //create private constructor to A singleton class is one which limits the number of objects creation to one.
+        private Single() { }
+
+
+        private static Single instance = new Single();
+
+        public static Single getInsatance() 
+            {
+            Console.WriteLine("Here is singleton");
+            return instance;
+           
+            }// this Instance is not similar to the instance that created above
+    
+            }
+   
+        
+       
+    
+        
+    }
+
